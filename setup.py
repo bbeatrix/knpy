3b7efc56ced896de641a833ff42a55c4254d35f7
dcs321 <dekanycsaba23@gmail.com>
2024-10-03 16:47:42 +0200

    Setup for turning it python package.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='knpy',        # Package name
+    version='0.1',            # Version number
+    packages=find_packages(), # Automatically find packages
+    install_requires=[],      # External dependencies (if any)
+    description='Package designed for machine learning in knot theory.',
+    long_description=open('README.md').read(),  # Detailed description from README
+    long_description_content_type='text/markdown',
+    url='https://github.com/username/repository',  # Your GitHub repository
+    author='Csaba Dékány',
+    author_email='dekanycsaba23@gmail.com',
+    license='MIT',            # License type
+)
