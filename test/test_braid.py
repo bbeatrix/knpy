7e8f2e5e33f8fc7e8917e5603f7a03e4c3049a68
dekanycs <dekanycs@syslerkft.onmicrosoft.com>
2024-10-17 09:29:50 +0200

    Unit test code writing started.

---
+++ test/test_braid.py
@@ -0,0 +1,20 @@
+import pytest
+
+class TestBraidClassBraidRelations:
+    def test_stabilization(self):
+        braid = Braid([1, -2, 3])
+        braid.stabilization()
+        self.assertEqual(braid.values()[0], 5)
+        self.assertEqual(braid.values()[1][-1], 3)
+        self.assertEqual(len(braid.values()[1]), 4)
+
+    def test_destabilization(self):
+        braid = Braid([1, -2, 3])
+        braid.destabilization()
+        self.assertEqual(braid.values()[0], 3)
+        self.assertEqual(len(braid.values()[1]), 2)
+
+    def test_destabilization_exception(self):
+        braid = Braid([-3 ,1, -2, 3])
+        with self.assertRaises(IllegalTransformationException):
+            braid.destabilization()
