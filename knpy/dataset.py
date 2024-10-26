f806c725091b9602cd42b9d04f6dd02635a1ae05
Benedek-Labancz <benedeklabancz@proton.me>
2024-10-26 14:16:41 +0200

    Fix generation of negative examples

--- knpy/dataset.py
+++ knpy/dataset.py
@@ -14,24 +14,12 @@ class KnotData(Dataset):
         return len(self.data)
     
     def transform(self, braid, num_moves):
-        performable_moves = braid.performable_moves()
-        print(performable_moves)
-        for move in performable_moves:
-            if move == partial(braid.conjugation, index=-1) or partial(braid.conjugation, index=1):
-                print('---conjugation')
-                move = partial(braid.conjugation, random.randint(1, braid._n - 1), random.randint(0, braid._n + 1))
-            braid = move()
-            print(move, " performed: ", braid._braid)
-
-
-        '''
         for i in range(num_moves):
                 performable_moves = braid.performable_moves()
-                #move = random.choice(performable_moves)
+                move = random.choice(performable_moves)
                 if move == braid.conjugation:
                      move = partial(move, random.randint(1, braid._n - 1), random.randint(0, braid._n + 1))
                 braid = move()
-        '''
         return braid
 
     def __getitem__(self, id):
@@ -39,19 +27,28 @@ class KnotData(Dataset):
         orig_braid = Braid(self.data.iloc[id][0])
         print(orig_braid._braid)
         if random.randint(0,9) >= 5:
-            return ((self.transform(orig_braid, num_moves), self.transform(orig_braid, num_moves)), 1)
+            return ((self.transform(orig_braid, num_moves).to_torch(), self.transform(orig_braid, num_moves).to_torch()), 1)
         
         else:
             dist_id = random.randint(0, self.__len__())
             while dist_id == id:
                  dist_id = random.randint(0, self.__len__())
-            distinct_braid = self.data.iloc[dist_id]
+            distinct_braid = Braid(self.data.iloc[dist_id][0])
 
-            return ((self.transform(orig_braid, num_moves), self.transform(distinct_braid, num_moves)), 0)
+            return ((self.transform(orig_braid, num_moves).to_torch(), self.transform(distinct_braid, num_moves).to_torch()), 0)
             
+
+def collate_fn(data):
+    print(data)
+    
 
 dataset = KnotData("./data_knots/prime_knots_in_braid_notation.csv")
 
-test = dataset[32]
-print(test)
-print(test[0][0]._braid, test[0][1]._braid)
+loader = DataLoader(
+     dataset,
+     batch_size=32,
+     shuffle=False,
+     collate_fn=collate_fn
+)
+
+print(next(iter(loader)))
