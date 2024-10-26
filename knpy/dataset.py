5882d09a265b2ff6414883a83381c9933958b2c3
Benedek-Labancz <benedeklabancz@proton.me>
2024-10-26 15:21:38 +0200

    Added collate function; bug fixes

--- knpy/dataset.py
+++ knpy/dataset.py
@@ -1,5 +1,6 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
 import pandas as pd
 import random
 from functools import partial
@@ -17,17 +18,14 @@ class KnotData(Dataset):
         for i in range(num_moves):
                 performable_moves = braid.performable_moves()
                 move = random.choice(performable_moves)
-                if move == braid.conjugation:
-                     move = partial(move, random.randint(1, braid._n - 1), random.randint(0, braid._n + 1))
                 braid = move()
         return braid
 
     def __getitem__(self, id):
         num_moves = random.randint(0, self.max_transform)
         orig_braid = Braid(self.data.iloc[id][0])
-        print(orig_braid._braid)
         if random.randint(0,9) >= 5:
-            return ((self.transform(orig_braid, num_moves).to_torch(), self.transform(orig_braid, num_moves).to_torch()), 1)
+            return ([self.transform(orig_braid, num_moves).to_torch(), self.transform(orig_braid, num_moves).to_torch()], 1)
         
         else:
             dist_id = random.randint(0, self.__len__())
@@ -35,20 +33,23 @@ class KnotData(Dataset):
                  dist_id = random.randint(0, self.__len__())
             distinct_braid = Braid(self.data.iloc[dist_id][0])
 
-            return ((self.transform(orig_braid, num_moves).to_torch(), self.transform(distinct_braid, num_moves).to_torch()), 0)
+            return ([self.transform(orig_braid, num_moves).to_torch(), self.transform(distinct_braid, num_moves).to_torch()], 0)
             
 
 def collate_fn(data):
-    print(data)
-    
+    max_length = 0
+    for i in range(len(data)):
+         for j in [0, 1]:
+              length = len(data[i][0][j])
+              if length > max_length:
+                   max_length = length
 
-dataset = KnotData("./data_knots/prime_knots_in_braid_notation.csv")
+    for i in range(len(data)):
+         for j in [0, 1]:
+              data[i][0][j] = F.pad(data[i][0][j], (0, max_length - len(data[i][0][j])))
 
-loader = DataLoader(
-     dataset,
-     batch_size=32,
-     shuffle=False,
-     collate_fn=collate_fn
-)
+    first = torch.stack([data[i][0][0] for i in range(len(data))])
+    second = torch.stack([data[i][0][1] for i in range(len(data))])
+    labels = torch.tensor([data[i][1] for i in range(len(data))])
 
-print(next(iter(loader)))
+    return first, second, labels
