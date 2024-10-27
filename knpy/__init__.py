8db3539cc8e870fc67e0c0d6e2e515fdace792fd
sulan <sulyok.andras.attila@itk.ppke.hu>
2024-10-27 22:27:17 +0100

    braid: optionally load the faster Braid implementation

--- knpy/__init__.py
+++ knpy/__init__.py
@@ -1,2 +1,7 @@
-from .braid import Braid
+import os as _os
+
+if _os.environ.get("KNPY_FAST_BRAID", "no").lower() in ["on", "yes", "true", "1"]:
+    from .braid_vec import Braid
+else:
+    from .braid import Braid
 from .exceptions import IllegalTransformationException, InvalidBraidException, IndexOutOfRangeException
