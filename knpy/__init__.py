2577dcd308f1287a023614517931fbb26e596c9d
SzilBalazs <notyourbusiness@gmail.com>
2024-10-28 18:13:48 +0100

    Make default more clear

--- knpy/__init__.py
+++ knpy/__init__.py
@@ -1,7 +1,7 @@
 import os as _os
 
 from .exceptions import IllegalTransformationException, InvalidBraidException, IndexOutOfRangeException
-if _os.environ.get("KNPY_FAST_BRAID", "no").lower() in ["on", "yes", "true", "1"]:
+if _os.environ.get("KNPY_FAST_BRAID", default="no").lower() in ["on", "yes", "true", "1"]:
     from .braid_vec import Braid
 else:
     from .braid import Braid
