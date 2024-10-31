72b043eb90c3e28615d3623af47660c47d6cb255
SzilBalazs <notyourbusiness@gmail.com>
2024-10-31 11:03:00 +0100

    Add better benchmark and set up multiprocessing

--- main.py
+++ main.py
@@ -5,29 +5,10 @@ import heapq
 import time
 import matplotlib.pyplot as plt
 import tqdm
+import numpy as np
 
 def get_time() -> int:
     return int(round(time.time() * 1000))
-
-def gen_random_move(braid: Braid) -> Braid:
-    moves = braid.performable_moves()
-    return random.choice(moves)
-
-def gen_random_relation2(braid: Braid) -> Braid:
-    moves = braid.performable_rel2()
-    return random.choice(moves)
-
-def gen_random_collapse(braid: Braid) -> Braid:
-    moves = braid.performable_collapse()
-    return random.choice(moves)
-    
-def gen_eq_braid(braid: Braid, depth: int) -> Braid:
-    for i in tqdm.tqdm(range(depth)):
-        mv = gen_random_move(braid)
-        braid = mv()
-        for j in range(100): braid = gen_random_relation2(braid)()
-    for j in range(100): braid = gen_random_collapse(braid)()
-    return braid
 
 def hard(n):
     return Braid([i for i in range(1, 2*n+1)] + [-i for i in range(1, 2*n+1)])
@@ -55,14 +36,6 @@ def minimal_crossing(starting_braid: Braid, execution_time: int):
         x = braid.notation(False)
         x.flags.writeable = False
         return hash(x.tobytes())
-    
-    # def calculate_heuristic(braid: Braid):
-    #     res = len(braid)
-    #     x = braid.notation(False)
-    #     for i in range(len(x)):
-    #         for j in range(min(10, i)):
-    #             res += abs(x[i-j]) > abs(x[i])
-    #     return res
 
     def add_node(braid: Braid, new_depth: int):
         fingerprint = get_fingerprint(braid)
@@ -71,15 +44,13 @@ def minimal_crossing(starting_braid: Braid, execution_time: int):
             heapq.heappush(pq, (braid.calculate_heuristic() * 5 + new_depth, braid))
 
     add_node(starting_braid, 0)
-    while len(pq) > 0 and len(best) > 12:
+    while len(pq) > 0 and len(best) > 1:
         it += 1
         if it % 100 == 0:
-            pq = pq[:10000]
             now = get_time()
             if now - start_time > execution_time:
                 break
         x = heapq.heappop(pq)
-        order_operator: int = x[0]
         braid: Braid = x[1]
         depth: int = dist[get_fingerprint(braid)]
         for mv in braid.performable_moves():
@@ -89,37 +60,40 @@ def minimal_crossing(starting_braid: Braid, execution_time: int):
             add_node(new_braid, depth + 1)
     return best
 
-# score = 0
-# max_score = 0
-# braids = []
-# for i in range(20):
-#     x = gen_eq_braid(Braid("11n_16"), random.randint(40, 100))
-#     braids.append(x)
-#     braid = minimal_crossing(x, 5000)
-#     print(len(braid))
-#     max_score += len(x)-12
-#     score += len(braid)-12
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import tqdm
+import matplotlib.pyplot as plt
 
-score = 0
-max_score = 0
-X = []
-Y = []
-Z = []
-for i in tqdm.tqdm(benchmark_braids):
-    x = Braid(benchmark_braids[i][0])
-    braid = minimal_crossing(x, 1000)
-    max_score += len(x)-12
-    score += len(braid)-12
-    X.append(len(x))
-    Y.append(len(braid))
-    Z.append("orange" if int(i.split("_")[0]) >= 20  else "green")
-print("Score =", 1-score/max_score)
-plt.vlines(12, 0, 100, colors='red', linestyles='dashed', label='True crossing number')
-plt.title("Crossing number prediction of 11n_16 equivalent knots")
-plt.scatter(Y, X, c=Z)
-plt.xlim(0, 100)
-plt.ylim(0, 100)
-plt.xlabel("Predicted crossing number")
-plt.ylabel("Input crossing number")
-plt.legend()
-plt.show()
+def compute_braid(key):
+    x = Braid(benchmark_braids[key][0])
+    braid = minimal_crossing(x, 200000)
+    return len(x), len(braid)
+
+if __name__ == "__main__":
+    score = 0
+    max_score = 0
+    X = []
+    Y = []
+    Z = []
+
+    with ProcessPoolExecutor(max_workers=2) as executor:
+        futures = {executor.submit(compute_braid, key): key for key in benchmark_braids}
+
+        for future in tqdm.tqdm(as_completed(futures), total=len(benchmark_braids)):
+            x_len, braid_len = future.result()
+            max_score += x_len - 1
+            score += braid_len - 1
+            X.append(x_len)
+            Y.append(braid_len)
+            Z.append("blue")
+
+    print("Score =", 1 - score / max_score)
+
+    plt.title("Crossing number prediction of 11n_16 equivalent knots")
+    plt.scatter(Y, X, c=Z)
+    plt.xlim(0, 100)
+    plt.ylim(0, 100)
+    plt.xlabel("Predicted crossing number")
+    plt.ylabel("Input crossing number")
+    plt.legend()
+    plt.show()
